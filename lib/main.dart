a62955fc9e66f213322925285dfa873fd267aeea
Leonardo Prado Suzin <leonardopradosuzin@gmail.com>
2023-10-07 11:02:45 -0300

    MAterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-      child: Center(
-        child: Text(
-          'Leonardo',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.0),
-        ),
-      ),
-    ),
-  );
+  runApp(const AppWidget(
+    title: 'Leonardo',
+  ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Container(
+          child: Center(child: Text('Leonardo')),
+        ));
+  }
 }
